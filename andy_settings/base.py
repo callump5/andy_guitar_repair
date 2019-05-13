f2edde90d48583872146862daed3283e565d1274
Callum <clpullinger@gmail.com>
2019-05-14 00:52:55 +0100

    adding live run files

--- andy_settings/base.py
+++ andy_settings/base.py
@@ -139,15 +139,3 @@ AWS_QUERYSTRING_AUTH = False     # don't add complex authentication-related quer
 
 AWS_STORAGE_BUCKET_NAME = 'action-guitar'
 
-
-CORS_REPLACE_HTTPS_REFERER      = False
-HOST_SCHEME                     = "http://"
-SECURE_PROXY_SSL_HEADER         = None
-SECURE_SSL_REDIRECT             = False
-SESSION_COOKIE_SECURE           = False
-CSRF_COOKIE_SECURE              = False
-SECURE_HSTS_SECONDS             = None
-SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
-SECURE_FRAME_DENY               = False
-
-
