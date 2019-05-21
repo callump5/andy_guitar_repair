d09ff7ee6a1f41e23c2afefd6d1a9196f1ee03cb
Callum <clpullinger@gmail.com>
2019-05-21 11:36:08 +0100

    responsive design

--- andy_site/models.py
+++ andy_site/models.py
@@ -64,7 +64,7 @@ class PricingItem(models.Model):
     price = models.DecimalField(max_digits=7, decimal_places=2)
 
     def __str__(self):
-        return  str(self.cate.service) + ' - ' + self.name
+        return self.name
 
     class Meta:
         verbose_name = 'Service Price'
